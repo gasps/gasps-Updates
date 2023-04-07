fc96a25657c4dd60a73b9d3286337bd7c1d1479c
gasps <60119170+gasps@users.noreply.github.com>
2023-04-06 21:02:24 -0400

    Add files via upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import tailwind from "@astrojs/tailwind";
 import robotsTxt from "astro-robots-txt";
 // https://astro.build/config
 export default defineConfig({
-  site: "https://blog.sirius.menu/",
+  site: "https://blog.gasp.lol/",
   integrations: [
     mdx(),
     sitemap(),
